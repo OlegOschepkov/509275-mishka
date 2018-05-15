3c72f034aa3451fc26a4d39b199bfc1b2ed7d378
OlegOschepkov <36763883+OlegOschepkov@users.noreply.github.com>
2018-05-15 17:46:27 +0400

    #14 Оптимизировать векторную графику - svg исправлены

--- gulpfile.js
+++ gulpfile.js
@@ -29,4 +29,5 @@ gulp.task("serve", ["style"], function () {
 
   gulp.watch("source/less/**/*.less", ["style"]);
   gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/img/*.*").on("change", server.reload);
 });
